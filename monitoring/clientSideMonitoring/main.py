3f7ea2ceb43cf64ce00de1027acbe18716c84902
exersalza <squidodle44@gmail.com>
2022-12-14 10:40:31 +0100

    init project

---
+++ monitoring/clientSideMonitoring/main.py
@@ -0,0 +1,18 @@
+"""
+
+capture stuff over time to calc an average
+
+"""
+import os
+import sys
+import psutil
+
+
+def main() -> int:
+
+
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
---
+++ monitoring/testApi/main.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": ""}
+
+
+@app.get("/v1/burn/{_id}/{comp}")
+async def _comp(_id: int, comp: str, value: str = ''):
+    print(_id, comp, value)
