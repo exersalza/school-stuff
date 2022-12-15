7bb17d016fde7f5c63bb3eea00fe6b98504a92e8
exersalza <squidodle44@gmail.com>
2022-12-15 10:54:32 +0100

    linux test

--- monitoring/clientSideMonitoring/main.py
+++ monitoring/clientSideMonitoring/main.py
@@ -4,51 +4,64 @@ capture stuff over time to calc an average
 
 """
 import json
+import sys
+
 import psutil
 
 from knxrcore.logger.logger import Logger, LogLevel
 
 
 def main() -> int:
-    c: dict
+    while True:
+        c: dict
 
-    with open('./config.json', 'r') as f:
-        c = json.load(f)
+        with open('./config.json', 'r') as f:
+            c = json.load(f)
 
-    logger = Logger(LogLevel.INFO, log_file='monitoring.log', api_url=c['api'])
+        logger = Logger(LogLevel.INFO, log_file='monitoring.log', api_url=c['api'])
 
-    # limits
-    cpu_limits = (c['hard'].get('cpu', 95), c['hard'].get('cpu', 80))
-    ram_limits = (c['hard'].get('ram', 90), c['hard'].get('ram', 80))
-    drive = (c['hard'].get('drive', 90), c['hard'].get('drive', 70))
+        # limits
+        cpu_limits = (c['hard'].get('cpu', 95), c['hard'].get('cpu', 80))
+        ram_limits = (c['hard'].get('ram', 90), c['hard'].get('ram', 80))
+        drive = (c['hard'].get('drive', 90), c['hard'].get('drive', 70))
 
-    cpu_usage = psutil.cpu_percent(interval=5)
-    mem = psutil.virtual_memory()
-    disc = psutil.disk_usage('/')
+        cpu_usage = psutil.cpu_percent(interval=1)
+        mem = psutil.virtual_memory()
 
-    stats = {
-        'cpu': {
-            'usage': cpu_usage
-        },
-        'ram': {
-            'usage': mem.percent,
-            'available': float(f'{mem.available / 1000000000:.3f}')
-        },
-        'disc': {
-            'usage': disc.percent,
-            'left': float(f'{disc.free / 1000000000:.3f}'),
-            'used': float(f'{disc.used / 1000000000:.3f}')
+        stats = {
+            'cpu': {
+                'usage': cpu_usage
+            },
+            'ram': {
+                'usage': mem.percent,
+                'available': float(f'{mem.available / 1000000000:.3f}')
+            },
+            'disc': {}
         }
-    }
-    print(stats)
 
-    if stats['cpu'].get('usage') >= cpu_limits[1]:
-        if stats['cpu'].get('usage') >= cpu_limits[0]:
-            pass
-    logger.error('Cpu has exceeded its hard limits').get_api('burn/1337/cpu', value=stats['cpu'].get('usage'))
+        for i, (v, *_) in enumerate(psutil.disk_partitions()):
+            temp = psutil.disk_usage(v)
+
+            stats['disc'].update({
+                i: {
+                    'usage': temp.percent,
+                    'free': float(f'{temp.free / 1000000000:.3f}'),
+                    'used': float(f'{temp.used / 1000000000:.3f}')
+                }
+            })
+
+        if stats['cpu'].get('usage') >= cpu_limits[1]:
+            if stats['cpu'].get('usage') >= cpu_limits[0]:
+                logger.error('Cpu has exceeded its hard limits').get_api('burn/1337/cpu',
+                                                                         value=stats['cpu'].get('usage'), limit='hard')
+                continue
+
+            logger.warning('Cpu has exceeded its soft limits').get_api('burn/1337/cpu',
+                                                                       value=stats['cpu'].get('usage'), limit='soft')
+            continue
 
     return 0
 
 
 if __name__ == '__main__':
-    exit(main())
+    sys.exit(main())
