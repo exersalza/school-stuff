0e944bdc91d9d89b331738e06915f90e6d327037
exersalza <squidodle44@gmail.com>
2022-12-20 10:59:32 +0100

    added tests

--- test_monitoring.py
+++ test_monitoring.py
@@ -1,6 +1,20 @@
+import subprocess
+
+from knxrcore.logger import Logger
+
 import monitoring.clientSideMonitoring.utils as u
 
 
 def test_is_linux():
     assert u.is_linux(), 'Should be linux'
 
+
+def test_check_for_new_login():
+    res = subprocess.run('last | grep "julian" | head -n 1'.split(), check=True, capture_output=True)
+    _, status_code = u.check_for_new_logins(b'', Logger(5), res)
+
+    assert status_code, 'Should be 0'
+
+
+if __name__ == '__main__':
+    test_check_for_new_login()
